a2a6a4f699f3fdf98510049f639ae072b3585efb
Rafael Durelli <rafa.durelli@gmail.com>
2014-02-11 10:28:13 +0100

    Primeiro commit. COlocando todos os arquivos juntos

---
+++ Planejamento/SBES_Ferramentas.mm
@@ -0,0 +1,98 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1391764003898" ID="ID_1029241040" MODIFIED="1391765598980" TEXT="SBES_Ferramentas">
+<node CREATED="1334237844059" ID="ID_919534095" MODIFIED="1391764104893" POSITION="right" TEXT="abstract">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1334251306242" ID="ID_705460871" MODIFIED="1334251772443" TEXT="Background">
+<icon BUILTIN="yes"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1334251452433" ID="ID_188126542" MODIFIED="1334251779114" TEXT="Problem">
+<icon BUILTIN="yes"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1334251333835" ID="ID_1234340780" MODIFIED="1334251852779" TEXT="Methods">
+<icon BUILTIN="yes"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1334251314485" ID="ID_983625358" MODIFIED="1334251789339" TEXT="Objectives">
+<icon BUILTIN="yes"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1334251341541" ID="ID_841360306" MODIFIED="1334251899523" TEXT="Results">
+<icon BUILTIN="yes"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1334251348866" ID="ID_1473740727" MODIFIED="1334251899523" TEXT="Conclusion">
+<icon BUILTIN="yes"/>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1391764115146" ID="ID_92201347" MODIFIED="1391764730448" POSITION="right" TEXT="Introduction">
+<node CREATED="1391764137963" ID="ID_697418175" MODIFIED="1391765811479" TEXT="Contextualizacao">
+<node CREATED="1391764164851" ID="ID_764998638" MODIFIED="1391764168770" TEXT="REfactoring"/>
+<node CREATED="1391764142089" ID="ID_421489691" MODIFIED="1391764164370" TEXT="MDD">
+<node CREATED="1391764169462" ID="ID_1495680960" MODIFIED="1391764178630" TEXT="Model-Driven-Refactorin"/>
+</node>
+<node CREATED="1391764182179" ID="ID_165254445" MODIFIED="1391764193309" TEXT="ADM">
+<node CREATED="1391766170293" ID="ID_712992342" MODIFIED="1391766173563" TEXT="KDM"/>
+</node>
+</node>
+<node CREATED="1391764197811" ID="ID_792210377" MODIFIED="1391764523949" TEXT="Problem">
+<node CREATED="1391764402434" ID="ID_1483976208" MODIFIED="1391764418285" TEXT="Analysis varius banch of the system">
+<node CREATED="1391764499909" ID="ID_216833177" MODIFIED="1391764501857" TEXT="reengineers would greatly benefit from the possibility to assess different choices, in practice they mostly rely on experience or intuition because of the lack of approaches providing com- parison between possible variations of a change."/>
+</node>
+<node CREATED="1391764420714" ID="ID_1477166986" MODIFIED="1391764435367" TEXT="realize comparation between the system">
+<node CREATED="1391764506126" ID="ID_1280623283" MODIFIED="1391764518156" TEXT="Therefore, software reengineers do not have the possibility to easily apply analyses on different version branches of a system and compare them to pick up the most adequate changes"/>
+</node>
+</node>
+<node CREATED="1391764524431" ID="ID_630435838" MODIFIED="1391764541803" TEXT="Motivation"/>
+<node CREATED="1391764542815" ID="ID_1218106776" MODIFIED="1391764562282" TEXT="Metodologia"/>
+<node CREATED="1391764562877" ID="ID_717087793" MODIFIED="1391764572527" TEXT="Objetivo"/>
+<node CREATED="1391764573099" ID="ID_1683255521" MODIFIED="1391764577089" TEXT="Estrutura do artigo"/>
+</node>
+<node CREATED="1391764732196" ID="ID_1164348378" MODIFIED="1391764868813" POSITION="right" TEXT="Ferramenta">
+<node CREATED="1391764751340" ID="ID_1311735365" MODIFIED="1391764770448" TEXT="Transformar o Java para JavaModel -PSM"/>
+<node CREATED="1391764772228" ID="ID_1190705387" MODIFIED="1391764782446" TEXT="Transformar o JavaModel para KDM - PIM"/>
+<node CREATED="1391764788139" ID="ID_1125716017" MODIFIED="1391764818303" TEXT="Aplicar as refatoracoes no PIM">
+<node CREATED="1391764818304" ID="ID_383011991" MODIFIED="1391764828441" TEXT="Validar com metricas - verificar com o HONDA"/>
+</node>
+<node CREATED="1391764829909" ID="ID_1102403226" MODIFIED="1391764851682" TEXT="Transformar o KDM para JavaModel"/>
+<node CREATED="1391764852142" ID="ID_1074135830" MODIFIED="1391764865434" TEXT="Transformar o JavaModel em arquivos .java"/>
+</node>
+<node CREATED="1391764870951" ID="ID_926965917" MODIFIED="1391764877463" POSITION="right" TEXT="Arquitetura da Ferramenta"/>
+<node CREATED="1391765016867" ID="ID_1792984054" MODIFIED="1391765020226" POSITION="right" TEXT="Related Work">
+<node CREATED="1391765036855" ID="ID_120557256" MODIFIED="1391765049304" TEXT="Model Driven Refactoring">
+<node CREATED="1391765072474" ID="ID_1058284093" MODIFIED="1391765113229" TEXT="Refactoring OCL Annotated UML Class Diagrams"/>
+<node CREATED="1391765115359" ID="ID_453796753" MODIFIED="1391765136775" TEXT="Towards a Multi-view Approach to Model-driven Refactoring"/>
+</node>
+<node CREATED="1391765050085" ID="ID_1408092493" MODIFIED="1391765163397" TEXT="FAST e ORION?"/>
+</node>
+<node CREATED="1391765020853" ID="ID_1765332023" MODIFIED="1391765026063" POSITION="right" TEXT="Concluding Remarks">
+<node CREATED="1391765178677" ID="ID_138171535" MODIFIED="1391765183226" TEXT="Resumo final do que foi feito"/>
+<node CREATED="1391765183773" ID="ID_926877775" MODIFIED="1391765287072" TEXT="Facilitar o engenheiro de software a realizar refatora&#xe7;ao no modelo ai invez do codigo fonte.">
+<node CREATED="1391765222057" ID="ID_1909766832" MODIFIED="1391765232497" TEXT="Podem comparar versoes mais facilmente"/>
+<node CREATED="1391765242019" ID="ID_1351260323" MODIFIED="1391765256808" TEXT="Podem assim identificar qual foi a mudanca mais adequada para aquele sistema"/>
+</node>
+<node CREATED="1391765287636" ID="ID_821835883" MODIFIED="1391765304009" TEXT="Essa ferramenta esta envolvida em um projeto maior.. (Daniel, Honda, etc.)"/>
+<node CREATED="1391765328322" ID="ID_1636583282" MODIFIED="1391765485614" TEXT="Limitacoes">
+<node CREATED="1391765332899" ID="ID_380216798" MODIFIED="1391765425465" TEXT="Poucos parsers transformam o c&#xf3;digo-fonte para o KDM">
+<node CREATED="1391765405723" ID="ID_1667049385" MODIFIED="1391765414500" TEXT="Nessarios ourtos parsers"/>
+</node>
+<node CREATED="1391765426318" ID="ID_1322004747" MODIFIED="1391765439714" TEXT="Criar outras refatoracoes de granularidades maiores"/>
+</node>
+<node CREATED="1391765486211" ID="ID_1209630763" MODIFIED="1391765495917" TEXT="Trabalhos Futuros">
+<node CREATED="1391765499346" ID="ID_948672248" MODIFIED="1391765515199" TEXT="Criar outros refatoracoes de granularidades maiores."/>
+<node CREATED="1391765516459" ID="ID_48376916" MODIFIED="1391765523418" TEXT="Realizar experimentos"/>
+<node CREATED="1391765531315" ID="ID_4125919" MODIFIED="1391765584110" TEXT="Melhorias na ferramenta">
+<node CREATED="1391765539173" ID="ID_570262427" MODIFIED="1391765542733" TEXT="Melhorar a forma de uso"/>
+<node CREATED="1391765545337" ID="ID_577493578" MODIFIED="1391765545337" TEXT=""/>
+</node>
+</node>
+</node>
+<node CREATED="1391765605659" ID="ID_1108664207" MODIFIED="1391765630450" POSITION="right" TEXT="Agradecimentos">
+<node CREATED="1391765620540" ID="ID_1927359251" MODIFIED="1391765624533" TEXT="Agradecer as bolsas"/>
+</node>
+<node CREATED="1391765630880" ID="ID_214909596" MODIFIED="1391765633825" POSITION="right" TEXT="references."/>
+</node>
+</map>
